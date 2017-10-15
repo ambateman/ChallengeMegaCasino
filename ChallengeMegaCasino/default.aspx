fb519987fb39d663af3bc7dad6d242091eea3148
ambateman <tmbateman@hotmail.com>
2017-10-14 18:40:33 -0700

    MegaCasinoChallenge code

---
+++ ChallengeMegaCasino/default.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeMegaCasino._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 57%;
+        }
+        .auto-style3 {
+            height: 167px;
+            width: 153px;
+        }
+        .auto-style4 {
+            height: 167px;
+            width: 156px;
+        }
+        .auto-style5 {
+            height: 167px;
+            width: 96px;
+        }
+        .auto-style6 {
+            width: 528px;
+        }
+    </style>
+</head>
+<body style="width: 515px">
+    <form id="form1" runat="server" class="auto-style6">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/images/Cherry.png" Width="174px" />
+                </td>
+                <td class="auto-style4">
+                    <asp:Image ID="Image2" runat="server" ImageUrl="~/images/Orange.png" />
+                </td>
+                <td class="auto-style5">
+                    <asp:Image ID="Image3" runat="server" ImageUrl="~/images/Seven.png" />
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        You Bet:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnLever" runat="server" Text="Pull The Lever!" OnClick="btnLever_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+    </form>
+    <p>
+        &nbsp;</p>
+    <p>
+        1 Cherry - x2 Your Bet</p>
+    <p>
+        2 Cherries - x3 Your Bet</p>
+    <p>
+        3 Cherries - x4 Your Bet</p>
+    <p>
+        3 7&#39;s -- Jackpot!&nbsp; -- 100x Your Bet</p>
+    <p>
+        HOWEVER...if there&#39;s even one BAR you win nothing</p>
+</body>
+</html>
---
+++ ChallengeMegaCasino/default.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeMegaCasino
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        Random slots = new Random();
+        int left;
+        int middle;
+        int right;
+        int BetMultiplier;
+        double yourBet;
+        double bank;
+
+        string[] slotValues = new string[]{ "/images/Bar.png", "/images/Cherry.png", "/images/Seven.png", "/images/Clover.png",
+            "/images/Diamond.png", "/images/HorseShoe.png", "/images/Bell.png", "/images/Lemon.png",
+            "/images/Orange.png", "/images/Plum.png", "/images/Strawberry.png", "/images/Watermelon.png" };
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                bank = 100.00;
+                ViewState.Add("bank",bank);
+   
+                bankMessage();
+            }
+        }
+
+        protected void btnLever_Click(object sender, EventArgs e)
+        {
+            //This takes the place of our lever for the slot machine.
+            //All this does is generate 3 random numbers to be used to decide 
+            //if fake money is won or lost.
+            left = slots.Next(0, 11);
+            middle = slots.Next(0, 11);
+            right = slots.Next(0, 11);
+            updateSlots();
+            calcBet();
+            updateWinLoss();
+            bankMessage();
+
+        }
+
+        private void updateSlots()
+        {
+            //This changes the images of the slot machine slots
+
+            Image1.ImageUrl = slotValues[left];
+            Image2.ImageUrl = slotValues[middle];
+            Image3.ImageUrl = slotValues[right];
+        }
+
+
+
+        private void calcBet()
+        {
+            //This covers all of the possibilites
+            //for a payout. It sets the bet multiplier
+            //First, check for bars -- this is instant loss
+
+            if (left == 0 || middle == 0 || right == 0)
+            {
+                BetMultiplier = -1; 
+            }
+            else
+            {
+                BetMultiplier = Math.Max(set7Flag(), set1CherryFlag());
+
+            }
+
+        }
+
+
+        private int  set7Flag()
+        {
+            //Checks to see if all slots are lucky number seven.
+            if (left == 2 && middle == 2 && right == 2) return 100;
+            return -1; //means no soap
+        }
+
+        private int set1CherryFlag()
+        {
+            int cherryCount = 0;
+            if (left == 1) ++cherryCount;
+            if (middle == 1) ++cherryCount;
+            if (right == 1) ++cherryCount;
+            if (cherryCount > 0) return cherryCount + 1;
+            else return -1;  
+        }
+
+
+        private void updateWinLoss()
+        {
+            
+            if (double.TryParse(TextBox1.Text.ToString(), out yourBet))
+            {
+                ViewState["bank"] = (double)ViewState["bank"] + yourBet*BetMultiplier;
+                setWinLossMessage(yourBet*BetMultiplier);
+  
+            }
+
+        }
+
+        private void setWinLossMessage(double betResult)
+        {
+
+            if (betResult > 0)
+            {
+                resultLabel.Text = String.Format("You bet {0:C} ", yourBet) + String.Format(" and won {0:C}", betResult) + "!";
+            }
+            else
+            {
+                resultLabel.Text = String.Format("Sad day for you. You lost {0:C}", -1 * betResult) + " Better Luck Next Time";
+            }
+
+        }
+
+        private void bankMessage()
+        {
+            moneyLabel.Text = String.Format("Player's Money: {0:C},", (double)ViewState["bank"]);
+
+        }
+    }
+}
